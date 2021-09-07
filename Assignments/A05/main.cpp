d2ade85b66375ba84205a2939a41713203685f5b
Kolten <klpulliam44@gmail.com>
2021-09-07 09:19:52 -0500

    Start Up

--- Assignments/A05/main.cpp
+++ Assignments/A05/main.cpp
@@ -1,14 +1,23 @@
-/**
+/*****************************************************************************
 *                    
-*  Author:           Terry
-*  Title:            Linked List Example
-*  Course:           2143
+*  Author:           Kolten Pulliam
+*  Email:            klpulliam44@gmail.com
+*  Label:            A05
+*  Title:            Program 1 - MyVector Class
+*  Course:           2143-Object-Oriented-Programming
 *  Semester:         Fall 2021
 * 
 *  Description:
-*        Linked List Example 
+*        
 * 
-*/
+*  Usage:
+*        N/A
+* 
+*  Files:            (list of all source files used in this program)
+                      main.cpp
+                      MyVector.cpp
+                      MyVector.h
+*****************************************************************************/
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -158,3 +167,76 @@ int main() {
     LinkedList L2(B, size);         // Create 2nd instance of list
     L2.print();                     // Print list 2
 }
+
+/*
+Test
+int x = 0;
+
+MyVector v1;
+v1.pushFront(18);
+v1.pushFront(20);
+v1.pushFront(25);
+v1.pushRear(18);
+v1.pushRear(20);
+v1.pushRear(25);
+v1.print();
+// [25, 20, 18, 18, 20, 25]
+
+int A[] = {11,25,33,47,51};
+MyVector v2(A,5);
+v2.print();
+// [11, 25, 33, 47, 51]
+
+v2.pushFront(9);
+v2.inOrderPush(27);
+v2.pushRear(63);
+v2.print();
+// [9, 11, 25, 27, 33, 47, 51, 63]
+
+v1.pushRear(v2);
+v1.print();
+// [25, 20, 18, 18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
+
+x = v1.popFront();
+x = v1.popFront();
+x = v1.popFront();
+v1.print();
+// [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
+cout<<x<<endl;
+// 18
+
+x = v1.popRear();
+x = v1.popRear();
+x = v1.popRear();
+v1.print();
+// [18, 20, 25, 9, 11, 25, 27, 33]
+cout<<x<<endl;
+// 47
+
+x = v2.popAt(3);
+v2.print();
+// [9, 11, 25, 33, 47, 51, 63]
+cout<<x<<endl;
+// 27
+
+x = v2.find(51);
+cout<<x<<endl;
+// 5
+
+x = v2.find(99);
+cout<<x<<endl;
+// -1
+
+MyVector v3(v1);
+v3.print();
+// [18, 20, 25, 9, 11, 25, 27, 33]
+
+v3.pushFront(v2);
+v3.print();
+//[9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+
+MyVector v4("input.dat");
+v4.pushRear(v3);
+v4.print();
+// [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 23, 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+*/
